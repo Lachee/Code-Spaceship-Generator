da8c3b653abdac3adc6079eecf6f85ccfe39ea0e
Lachee <lachee@lacheedomain.netii.net>
2015-06-25 22:15:49 +1000

    Initial Commit
    Here is a very simple program

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace CodeSpaceshipGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.textbox_path = new System.Windows.Forms.TextBox();
+            this.btn_browse = new System.Windows.Forms.Button();
+            this.btn_generate = new System.Windows.Forms.Button();
+            this.checkbox_save = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(449, 516);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(468, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Code Location:";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // textbox_path
+            // 
+            this.textbox_path.Location = new System.Drawing.Point(481, 29);
+            this.textbox_path.Name = "textbox_path";
+            this.textbox_path.Size = new System.Drawing.Size(181, 20);
+            this.textbox_path.TabIndex = 2;
+            // 
+            // btn_browse
+            // 
+            this.btn_browse.Location = new System.Drawing.Point(668, 27);
+            this.btn_browse.Name = "btn_browse";
+            this.btn_browse.Size = new System.Drawing.Size(75, 23);
+            this.btn_browse.TabIndex = 3;
+            this.btn_browse.Text = "Browse";
+            this.btn_browse.UseVisualStyleBackColor = true;
+            this.btn_browse.Click += new System.EventHandler(this.btn_browse_Click);
+            // 
+            // btn_generate
+            // 
+            this.btn_generate.Location = new System.Drawing.Point(481, 55);
+            this.btn_generate.Name = "btn_generate";
+            this.btn_generate.Size = new System.Drawing.Size(115, 23);
+            this.btn_generate.TabIndex = 4;
+            this.btn_generate.Text = "Generate Ship";
+            this.btn_generate.UseVisualStyleBackColor = true;
+            this.btn_generate.Click += new System.EventHandler(this.btn_generate_Click);
+            // 
+            // checkbox_save
+            // 
+            this.checkbox_save.AutoSize = true;
+            this.checkbox_save.Checked = true;
+            this.checkbox_save.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkbox_save.Location = new System.Drawing.Point(621, 59);
+            this.checkbox_save.Name = "checkbox_save";
+            this.checkbox_save.Size = new System.Drawing.Size(83, 17);
+            this.checkbox_save.TabIndex = 5;
+            this.checkbox_save.Text = "Save Image";
+            this.checkbox_save.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 540);
+            this.Controls.Add(this.checkbox_save);
+            this.Controls.Add(this.btn_generate);
+            this.Controls.Add(this.btn_browse);
+            this.Controls.Add(this.textbox_path);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.TextBox textbox_path;
+        private System.Windows.Forms.Button btn_browse;
+        private System.Windows.Forms.Button btn_generate;
+        private System.Windows.Forms.CheckBox checkbox_save;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CodeSpaceshipGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string path = "../../Form1.cs";
+
+            int lineHeight = 10;
+            int tabSize = 5;
+            
+            Color bkgColor = Color.FromArgb(34, 34, 34);
+            Color polyColor = Color.White;
+
+            GenerateImage(path, lineHeight, tabSize, bkgColor, polyColor);
+        }
+
+        public void GenerateImage(string path, int lineHeight, int tabSize, Color bkgColor, Color polyColor, int imageWidth = 256)
+        {
+            //Get all the points. Its length will always be the length of the image
+            Point[] pointsA = GetTabPoints(path, imageWidth, 0, tabSize, lineHeight);
+            Point[] pointsB = GetTabPoints(path, imageWidth, 0, -tabSize, lineHeight);
+
+
+            //Prepare the bitmap
+            Bitmap image = new Bitmap(512, pointsA.Length * lineHeight);
+
+            //Background Color
+            //Color bkgColor = Color.FromArgb(34, 34, 34);
+            //Color polyColor = Color.White;
+            //Color polyColorOther = Color.White;
+
+            //Draw the polygon
+            using (Graphics graphics = Graphics.FromImage(image))
+            {
+                graphics.FillRectangle(new SolidBrush(bkgColor), 0, 0, image.Width - 1, image.Height - 1);
+                graphics.FillPolygon(new SolidBrush(polyColor), pointsA);
+                graphics.FillPolygon(new SolidBrush(polyColor), pointsB);
+            }
+
+            //Set the image
+            pictureBox1.Image = image;
+
+            //Save the image
+            if (checkbox_save.Checked)
+            {
+                string imageName = Path.GetFileName(path);
+                image.Save(imageName + ".bmp");
+            }
+        }
+
+        public Point[] GetTabPoints(string file, int x, int y, int xmod = 1, int ymod = 1)
+        {
+            if (!File.Exists(file))
+            {
+                //We cannot find the file, so return empty.
+                Console.WriteLine(file + " does not exist!");
+                return new Point[0];
+            }
+
+            //Prepare the points
+            List<Point> points = new List<Point>();
+
+            //Read the file lines
+            string[] lines = File.ReadAllLines(file);
+
+            //prepare the previous tab. This is used for when we come accross a empty line.
+            int preTabs = 0;
+
+            //Itterate through the lines
+            for (int i = 0; i < lines.Length; i++)
+            {
+                //Get the line
+                string ln = lines[i];
+
+                if (ln == "")
+                {
+                    //if the line is empty, use the previous one and continue
+                    string previousLine = i - 1 > 0 ? lines[i - 1] : "";
+                    string nextLine = i + 1 < lines.Length ? lines[i+1] : "";
+
+                    if (previousLine != "" && nextLine != "")
+                        points.Add(new Point(x + ((preTabs + 1) * xmod), y + (i * ymod)));
+                    else
+                        points.Add(new Point(x + (preTabs * xmod), y + (i * ymod)));
+
+
+                    continue;
+                }
+
+                //Count how many tabs there are
+                int tabs = GetLeadingWhitespaceLength(ln);
+
+                /*
+                Console.WriteLine();
+                Console.WriteLine("Line: " + ln);
+                Console.WriteLine("Number of Tabs: " + tabs);
+                Console.WriteLine();
+                */
+
+                //Has the tab count changed?
+                if (tabs != preTabs)
+                {
+                    //We must step from previous tabs to current tabs
+                    points.Add(new Point(x + preTabs * xmod, y + i * ymod));
+                }
+
+                //Add it to the point list
+                points.Add(new Point(x + tabs * xmod, y + i * ymod));
+
+                //Store it as the previous points
+                preTabs = tabs;
+            }
+
+            points.Add(new Point(x + preTabs * xmod, y + lines.Length * ymod));
+            points.Add(new Point(x , y + lines.Length * ymod));
+
+            //return the point list
+            return points.ToArray();
+        }
+
+        public int GetLeadingWhitespaceLength(string s, int tabLength = 4, bool trimToLowerTab = true)
+        {
+            if (s.Length < tabLength) return 0;
+
+            int whiteSpaceCount = 0;
+
+            while (whiteSpaceCount < s.Length && Char.IsWhiteSpace(s[whiteSpaceCount])) whiteSpaceCount++;
+
+            if (whiteSpaceCount < tabLength) return 0;
+
+            if (trimToLowerTab)
+            {
+                whiteSpaceCount -= whiteSpaceCount % tabLength;
+            }
+
+            return whiteSpaceCount;
+        }
+
+        private void btn_browse_Click(object sender, EventArgs e)
+        {
+            openFileDialog.ShowDialog();
+            textbox_path.Text = openFileDialog.FileName;
+        }
+
+        private void btn_generate_Click(object sender, EventArgs e)
+        {
+
+            if (!File.Exists(textbox_path.Text))
+            {
+                MessageBox.Show("The path '" + textbox_path.Text + "' does not exist!", "File Not Found", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            int lineHeight = 10;
+            int tabSize = 5;
+
+            Color bkgColor = Color.FromArgb(34, 34, 34);
+            Color polyColor = Color.White;
+
+            GenerateImage(textbox_path.Text, lineHeight, tabSize, bkgColor, polyColor);
+        }
+    }
+}
+
